6cbc4de5a7996236fd87e6bf12e64513cc9d90da
Muksin Muksin <75759731+muki01@users.noreply.github.com>
2023-11-07 23:01:06 +0200

    Add Read Troubleshoot Codes Function

--- OBD2_Protocols.ino
+++ OBD2_Protocols.ino
@@ -16,22 +16,35 @@ AltSoftSerial K_Serial;
 
 int SPEED, RPM, THROTTLE, COOLANT_TEMP, INTAKE_TEMP, VOLTAGE;
 
-uint8_t buffer[20];
+uint8_t result = 0;
+uint8_t resultBuffer[20];
 uint8_t initBuffer[20];
-int result = 0;
 
-const byte init_obd[4] = { 0xC1, 0x33, 0xF1, 0x81 };  // Init fast ISO14230
+int dtcs = 0;
+char dtc_Byte_1, dtc_Byte_2;
+String dtcBuffer[20];
+
+const byte init_obd[4] = { 0xC1, 0x33, 0xF1, 0x81 };    // Init fast ISO14230
+const byte read_DTCs[4] = { 0xC1, 0x33, 0xF1, 0x03 };   // Read Troubleshoot Codes
+const byte clear_DTCs[4] = { 0xC1, 0x33, 0xF1, 0x04 };  // Clear Troubleshoot Codes
 
 // Request data bytes for fast init (ISO14230)
+const byte read_PIDs_20[5] = { 0xC2, 0x33, 0xF1, 0x01, 0x00 };  // Read suported PIDs 0-20
+const byte read_PIDs_40[5] = { 0xC2, 0x33, 0xF1, 0x01, 0x20 };  // Read suported PIDs 20-40
+const byte read_PIDs_60[5] = { 0xC2, 0x33, 0xF1, 0x01, 0x40 };  // Read suported PIDs 40-60
+const byte read_PIDs_80[5] = { 0xC2, 0x33, 0xF1, 0x01, 0x60 };  // Read suported PIDs 60-80
+const byte read_PIDs_A0[5] = { 0xC2, 0x33, 0xF1, 0x01, 0x80 };  // Read suported PIDs 80-A0
+const byte read_PIDs_C0[5] = { 0xC2, 0x33, 0xF1, 0x01, 0xA0 };  // Read suported PIDs A0-C0
+const byte read_PIDs_E0[5] = { 0xC2, 0x33, 0xF1, 0x01, 0xC0 };  // Read suported PIDs A0-E0
 const byte speed_obd[5] = { 0xC2, 0x33, 0xF1, 0x01, 0x0D };
 const byte rpm_obd[5] = { 0xC2, 0x33, 0xF1, 0x01, 0x0C };
 const byte throttle_obd[5] = { 0xC2, 0x33, 0xF1, 0x01, 0x11 };
 const byte coolant_temp_obd[5] = { 0xC2, 0x33, 0xF1, 0x01, 0x05 };
 const byte intake_temp_obd[5] = { 0xC2, 0x33, 0xF1, 0x01, 0x0F };
 
-//byte gear_obd[6] = {0xC2, 0x33, 0xF1, 0x01, 0xA4, 0x8B};
-
 // Request data bytes for slow init (ISO9141 and KWP slow)
+// const byte read_DTCs[4] = { 0x68, 0x6A, 0xF1, 0x03 };
+// const byte clear_DTCs[4] = { 0x68, 0x6A, 0xF1, 0x04 };
 // const byte speed_obd[5] = { 0x68, 0x6A, 0xF1, 0x01, 0x0D };
 // const byte rpm_obd[5] = { 0x68, 0x6A, 0xF1, 0x01, 0x0C };
 // const byte throttle_obd[5] = { 0x68, 0x6A, 0xF1, 0x01, 0x11 };
@@ -43,6 +56,9 @@ void setup() {
   Serial.begin(9600);
   pinMode(K_line_RX, INPUT_PULLUP);
   pinMode(K_line_TX, OUTPUT);
+  // decodeDTC(0x01, 0x70);
+  // decodeDTC(0x01, 0x30);
+  // decodeDTC(0xC1, 0x58);
 }
 
 void loop() {
@@ -52,7 +68,9 @@ void loop() {
   if (init_success) {
     Serial.println("Init Success !!");
     while (1) {
-      read_K();
+      // read_K();
+      read_DTC();
+      delay(10000);
     }
   }
 
@@ -66,9 +84,9 @@ void read_K() {
   writeData(throttle_obd, sizeof(throttle_obd));
   readData();
 
-  if (buffer[10] == 0X11) {
-    THROTTLE = buffer[11] * 100 / 255;
-    // THROTTLE = buffer[11] * 100 / 180 - 14;
+  if (resultBuffer[10] == 0X11) {
+    THROTTLE = resultBuffer[11] * 100 / 255;
+    // THROTTLE = resultBuffer[11] * 100 / 180 - 14;
     Serial.print("Throttle: ");
     Serial.println(THROTTLE);
   }
@@ -77,8 +95,8 @@ void read_K() {
   // writeData(voltage_obd, sizeof(voltage_obd));
   // readData();
 
-  // if (buffer[10] == 0X42) {
-  //   VOLTAGE = (buffer[11] * 256 + buffer[12]) / 1000;
+  // if (resultBuffer[10] == 0X42) {
+  //   VOLTAGE = (resultBuffer[11] * 256 + resultBuffer[12]) / 1000;
   //   Serial.print("Voltage: ");
   //   Serial.println(VOLTAGE);
   // }
@@ -87,8 +105,8 @@ void read_K() {
   writeData(rpm_obd, sizeof(rpm_obd));
   readData();
 
-  if (buffer[10] == 0x0C) {
-    RPM = (buffer[11] * 256 + buffer[12]) / 4;
+  if (resultBuffer[10] == 0x0C) {
+    RPM = (resultBuffer[11] * 256 + resultBuffer[12]) / 4;
     Serial.print("RPM: ");
     Serial.println(RPM);
   }
@@ -97,8 +115,8 @@ void read_K() {
   writeData(coolant_temp_obd, sizeof(coolant_temp_obd));
   readData();
 
-  if (buffer[10] == 0x05) {
-    COOLANT_TEMP = buffer[11] - 40;
+  if (resultBuffer[10] == 0x05) {
+    COOLANT_TEMP = resultBuffer[11] - 40;
     Serial.print("Coolant Temp: ");
     Serial.println(COOLANT_TEMP);
   }
@@ -107,8 +125,8 @@ void read_K() {
   writeData(intake_temp_obd, sizeof(intake_temp_obd));
   readData();
 
-  if (buffer[10] == 0x0F) {
-    INTAKE_TEMP = buffer[11] - 40;
+  if (resultBuffer[10] == 0x0F) {
+    INTAKE_TEMP = resultBuffer[11] - 40;
     Serial.print("Intake Temp: ");
     Serial.println(INTAKE_TEMP);
   }
@@ -117,13 +135,49 @@ void read_K() {
   writeData(speed_obd, sizeof(speed_obd));
   readData();
 
-  if (buffer[10] == 0x0D) {
-    SPEED = buffer[11];
+  if (resultBuffer[10] == 0x0D) {
+    SPEED = resultBuffer[11];
     Serial.print("Speed: ");
     Serial.println(SPEED);
   }
 
   Serial.println();
+}
+
+void read_DTC() {
+  writeData(read_DTCs, sizeof(read_DTCs));
+  delay(REQUEST_DELAY);
+  result = K_Serial.available();
+  if (result > 0) {
+    // Serial.print("Buffer: ");
+    for (int i = 0; i < result; i++) {
+      resultBuffer[i] = K_Serial.read();
+      delay(READ_DELAY);
+      // Serial.print(resultBuffer[i], HEX);
+      // Serial.print(" ");
+    }
+    // Serial.println();
+    int length = result - 10;
+    for (int i = 0; i < length; i++) {
+      int index1 = 9 + i * 2;
+      int index2 = 9 + i * 2 + 1;
+      dtc_Byte_1 = resultBuffer[index1];
+      dtc_Byte_2 = resultBuffer[index2];
+      delay(READ_DELAY);
+
+      if (dtc_Byte_1 == 0 && dtc_Byte_2 == 0) {
+        Serial.println("Errors Ended");
+        return;
+      } else {
+        // Serial.print("Errors: "), Serial.print(dtc_Byte_1, HEX), Serial.print(" "), Serial.println(dtc_Byte_2, HEX);
+        decodeDTC(dtc_Byte_1, dtc_Byte_2);
+      }
+    }
+  }
+}
+
+void clear_DTC() {
+  writeData(clear_DTCs, sizeof(clear_DTCs));
 }
 
 bool init_KWP() {
@@ -143,11 +197,11 @@ bool init_KWP() {
       delay(READ_DELAY);
     }
   }
-  
   if (initBuffer[8] == 0xC1) {
     return true;
+  } else {
+    return false;
   }
-  return false;
 }
 
 bool init_ISO9141() {
@@ -211,12 +265,9 @@ void readData() {
   result = K_Serial.available();
   if (result > 0) {
     for (int i = 0; i < result; i++) {
-      buffer[i] = K_Serial.read();
+      resultBuffer[i] = K_Serial.read();
       delay(READ_DELAY);
-      // Serial.print(buffer[i], HEX);
-      // Serial.print(" ");
     }
-    // Serial.println();
   }
 }
 
@@ -226,4 +277,21 @@ byte calculateChecksum(byte data[], int length) {
     checksum += data[i];
   }
   return checksum % 256;
+}
+
+void decodeDTC(char input_byte1, char input_byte2) {
+  String ErrorCode = "";
+  const static char type_lookup[4] = { 'P', 'C', 'B', 'U' };
+  const static char digit_lookup[16] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' };
+
+  ErrorCode += type_lookup[(input_byte1 >> 6) & 0b11];
+  ErrorCode += digit_lookup[(input_byte1 >> 4) & 0b11];
+  ErrorCode += digit_lookup[input_byte1 & 0b1111];
+  ErrorCode += digit_lookup[input_byte2 >> 4];
+  ErrorCode += digit_lookup[input_byte2 & 0b1111];
+
+  dtcBuffer[dtcs] = ErrorCode;
+  Serial.print("Error Code: ");
+  Serial.println(dtcBuffer[dtcs]);
+  dtcs++;
 }
