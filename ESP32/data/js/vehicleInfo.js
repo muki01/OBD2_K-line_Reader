c3e1ac0404099d2a167ea4733ace0cfed30942ee
Muksin Muksin <muksin.muksin04@gmail.com>
2024-07-08 22:42:27 +0300

    Add Supported PIDs to Vehicle Info Page

--- ESP32/data/js/vehicleInfo.js
+++ ESP32/data/js/vehicleInfo.js
@@ -3,12 +3,29 @@ import { InitWebSocket, setMessageHandler } from "./webSocket.js";
 let wsStatus = document.getElementById("ws");
 let klStatus = document.getElementById("kl");
 
+let PIDsField = document.getElementById("PIDs");
+
 function handleWebSocketMessage(wsMessage) {
     if (wsMessage) {
         wsStatus.style.fill = "#00ff00";
         document.getElementById("VIN").innerHTML = wsMessage.VIN;
         document.getElementById("ID").innerHTML = wsMessage.ID;
         document.getElementById("IDNum").innerHTML = wsMessage.IDNum;
+        let PIDsArray = wsMessage.SuportedPIDs;
+        if (PIDsArray.length > 0) {
+            let PIDsString = "";
+            for (let i = 0; i < PIDsArray.length; i++) {
+                if (i === PIDsArray.length - 1) {
+                    PIDsString += PIDsArray[i];
+                } else {
+                    PIDsString += PIDsArray[i] + ", ";
+                }
+            }
+            PIDsField.innerHTML = PIDsString;
+        } else {
+            PIDsField.innerHTML = "Error";
+        }
+
         if (wsMessage.KLineStatus == true) {
             klStatus.style.fill = "#00ff00";
         } else {
